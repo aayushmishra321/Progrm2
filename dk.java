9ccd089024712a830cdf4d5d1bd6b6004d682405
aayushmishra321 <Maayush081@gmail.com>
2024-08-12 09:40:43 +0530

    Update dk.java

--- dk.java
+++ dk.java
@@ -1,6 +1,6 @@
 // This is a simple Java program that prints "Hello, World!" to the console.
 
-public class HelloWorld {
+public class dk {
     public static void main(String[] args) {
         // Print "Hello, World!" to the console
         System.out.println("Hello, World!");
